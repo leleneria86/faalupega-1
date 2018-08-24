ea155c9f304ed58e22f85f86170683ddd34258f1
fululele <37724798+fululele@users.noreply.github.com>
2018-08-24 16:54:21 -0600

    Update index.php

--- www/index.php
+++ www/index.php
@@ -19,7 +19,9 @@
 
 <div ng-app="MyApp" ng-controller="MyController" data-ng-init="load()">
 
-    <div class="col-sm-2" ng-hide="selected_grandchild">
+    <div class="col-sm-2" ng-hide="selected_nuu">
+
+        <div id="like_button_container"></div>
 
         <div>
             <br/>
@@ -27,45 +29,49 @@
         </div>
         <br/>
         <div>
-            <select id="parentSelect" name="parentSelect" class="form-control"
-                    ng-model="selected_parent"
-                    ng-change="onChangeParent()"
-                    ng-options="parent.name for parent in parents" style="width:200px">
+            <select id="motuSelect" name="motuSelect" class="form-control"
+                    ng-model="selected_motu"
+                    ng-change="onChangeMotu()"
+                    ng-options="motu.name for motu in motus" style="width:200px">
             </select>
         </div>
 
-        <div ng-show="selected_parent.content.length > 0">
+        <div ng-show="selected_motu.content.length > 0">
             <br/>
-            {{selected_parent.content}}
+            {{selected_motu.content}}
         </div>
 
-        <div ng-show="children_filtered.length > 0">
+        <div ng-show="itumalos_filtered.length > 0">
             <br/>
-            <select id="childSelect" name="childSelect" class="form-control"
-                    ng-model="selected_child"
-                    ng-change="onChangeEvent()"
-                    ng-options="child.name for child in children_filtered" style="width:200px">
+            <select id="itumaloSelect" name="itumaloSelect" class="form-control"
+                    ng-model="selected_itumalo"
+                    ng-change="onChangeItumalo()"
+                    ng-options="itumalo.name for itumalo in itumalos_filtered" style="width:200px">
             </select>
-            <span ng-show="selected_child.id != 0 && !show_child_content" style="cursor:pointer" ng-click="onClickChildContents()"><i class="fa fa-expand"></i></span>
-            <span ng-show="selected_child.id != 0 && show_child_content" style="cursor:pointer" ng-click="onClickChildContents()"><i class="fa fa-compress"></i></span>
+            <span ng-show="selected_itumalo.id != 0 && !show_itumalo_content" style="cursor:pointer" ng-click="onClickItumaloContents()"><i class="fa fa-expand"></i></span>
+            <span ng-show="selected_itumalo.id != 0 && show_itumalo_content" style="cursor:pointer" ng-click="onClickItumaloContents()"><i class="fa fa-compress"></i></span>
         </div>
 
-        <div ng-show="show_child_content && selected_child.content.length > 0">
+        <div ng-show="show_itumalo_content && selected_itumalo.content.length > 0">
             <br/>
-            <div ng-repeat="line in selected_child.content">
+            <div ng-repeat="line in selected_itumalo.content">
                 {{line}}
             </div>
         </div>
 
         <br/>
-        <div ng-show="selected_child || search_text" ng-repeat="grandchild in grandchildren_filtered | filter:{parent_id:selected_child.id,content:search_text}">
-            <span ng-click="onGrandchild(grandchild);" style="color:blue;cursor:pointer;font-size: large">{{grandchild.name}}</span>
+        <div ng-show="selected_itumalo || search_text" ng-repeat="nuu in nuus_filtered | filter:{motu_id:selected_itumalo.id,content:search_text}">
+            <span ng-click="onNuu(nuu);" style="color:blue;cursor:pointer;font-size: large">{{nuu.name}}</span>
         </div>
+
+        <!--div ng-show="selected_nuu || search_text" ng-repeat="greatgrandchild in greatgranditumalo_filtered | filter:{motu_id:selected_nuu.id,content:search_text}">
+            <span ng-click="onGrandchild(grandchild);" style="color:blue;cursor:pointer;font-size: large">{{greatgrandchild.name}}</span>
+        </div-->
     </div>
 
-    <div class="col-sm-12" ng-show="selected_grandchild">
+    <div class="col-sm-12" ng-if="selected_nuu">
         <br/>
-        <div ng-repeat="content in selected_grandchild.content">
+        <div ng-repeat="content in selected_nuu.content">
             <div ng-repeat="(key,lines) in content">
                 <br/>
                 <strong>{{key}}</strong>
@@ -75,9 +81,42 @@
             </div>
         </div>
         <br/>
+        <div ng-repeat="pitonuu in pitonuus_filtered">
+            <span ng-click="onPitonuu(pitonuu);" style="color:blue;cursor:pointer;font-size: large">{{pitonuu.name}}</span>
+            <div ng-if="pitonuu.expanded" ng-repeat="content in pitonuu.content">
+                <div ng-repeat="(key,lines) in content">
+                    <br/>
+                    <span ng-if="key != 'Fa\'alupega'"><strong>{{key}}</strong></span>
+                    <div ng-repeat="line in lines">
+                        {{line}}
+                    </div>
+                </div>
+                <br/>
+            </div>
+        </div>
+        <br/>
         <span ng-click="onBack();" style="color:blue;cursor:pointer">Back</span>
     </div>
-</div>
 
+    <!--div class="col-sm-12" ng-show="pitonuu_view">
+        <br/>
+        <div ng-repeat="content in selected_pitonuu.content">
+            <div ng-repeat="(key,lines) in content">
+                <br/>
+                <strong>{{key}}</strong>
+                <div ng-repeat="line in lines">
+                    {{line}}
+                </div>
+            </div>
+        </div>
+        <br/>
+        <span ng-click="onBackToNuu();" style="color:blue;cursor:pointer">Back</span>
+    </div-->
+</div>
+<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+<script src="js/components/like_button.js"></script>
 </body>
 </html>
+
