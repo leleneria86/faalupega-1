f3fd7c03713e716031e104a51260bc485e19de49
fululele <37724798+fululele@users.noreply.github.com>
2018-11-07 17:38:51 -0700

    Update MyService.js

--- www/js/services/MyService.js
+++ www/js/services/MyService.js
@@ -6,11 +6,6 @@ myApp.factory('MyService', function($http)
 
     service.load = function() {
 
-        // var data = [];
-        // data.children = service.listChildrenJson();
-        // data.parents = service.listParentJson();
-        // data.grandchildren = service.listGrandchildrenJson();
-
         return $http.get('/services/testapi.php').then(
 
             function(result) {
@@ -18,9 +13,6 @@ myApp.factory('MyService', function($http)
                 if (result.data) {
 
                     var data = result.data;
-                    //data.children = service.listChildrenJson();
-                    //data.parents = service.listParentJson();
-                    //data.grandchildren = service.listGrandchildrenJson();
 
                     return data;
                 } else {
@@ -28,50 +20,7 @@ myApp.factory('MyService', function($http)
                     return $q.reject();
                 }
             });
-
-        //return data;
     };
-
-    service.listParentJson = function() {
-
-        var data = [{id:0, name:"All", content:"",motu_id:null},
-                    {id:1000,name:"Tutuila",content:""},
-                    {id:2000,name:"Upolu",content:"Tumua ma Pule, Itu'au ma Alatau, Aiga i le Tai ma le Va'a o Fonoti"},
-                    {id:3000,name:"Savai'i",content:"Pule e ono"}];
-
-        return data;
-    };
-
-    // service.listChildrenJson = function() {
-    //
-    //     var data = [{id:0, name:"All", content:"",parent_id:null},
-    //                 {id:1,name:"Ma'oputasi",content:"Afio mai Ma'opu", parent_id:1},
-    //                 {id:2,name:"Sa'ole",content:"Upu a Sa'ole", parent_id:1},
-    //                 {id:3,name:"Three",content:"Child Three Desc", parent_id:2}];
-    //
-    //     return data;
-    // };
-
-    // service.listGrandchildrenJson = function() {
-    //
-    //     var data = [];
-    //     var as = new Aua();
-    //     data.push(as.getAuaContent());
-    //     data.push(as.getLeloaloaContent());
-    //
-    //     // var data = [{id:1,name:"Aua",
-    //     //                 content:[{"Fa'alupega":["Afio mai le Sa'ousoali'i o Unutoa, Le tama aitu ma tagata",
-    //     //                         "Le Matua or Lifau ma Liufau",
-    //     //                         "Susu mai Ali'i of le Faleono"]},
-    //     //                         {"Malaefono":["Paepaeulupo'o"]}],
-    //     //                 parent_id:1},
-    //     //             {id:2,name:"Leloaloa",
-    //     //                 content:"Afio mai Ma'opu(Faumuina ma Uti), Afio mai le ali'i o le Ao(Vaivao)",
-    //     //                 parent_id:1},
-    //     //             {id:3,name:"Amouli", content:"Afio mai Utu ma le Suafa o Gogo, Ali'i o le Faleono", parent_id:2}];
-    //
-    //     return data;
-    // };
 
     return service;
 });
