11afd9c2b4acc9e4c687b53b47cd0e746a6b78ce
fululele <37724798+fululele@users.noreply.github.com>
2019-01-03 10:44:56 -0700

    Update Savaii.php

--- www/services/classes/Savaii.php
+++ www/services/classes/Savaii.php
@@ -401,23 +401,23 @@ class Savaii
             "Maliu mai le mamalu o le nofo a ituaiga ia te oe le Alataua",
             "Tapa'au le fano, Nofoapule, ma le Matuafia"]];
         
-        //$content['pitonuus'][] = $this->getVaega($id);
+        $content['pitonuus'][] = $this->getVaega($id);
         return $content;
     }
     
     function getVaega($id) {
 
         $content = [];
-//         $content['id'] = $id +1;
-//         $content['motu_id'] = $id;
-//         $content['itumalo_id'] = $this->salega_id;
-//         $content['name'] = "Vaega";
-//         $content['content'][] = ["Vaega" => 
-//             ["Susu mai le paia i le nofo fealoa'i ia Sa Asiata (Asiatā)",
-//             "lau tofa le vaa i malae le na tausi vaa tele",
-//             "Susu mai le vaa i ti",
-//             "Susu mai le suafa Fauatea ma le aiga sa Valomua",
-//             "Maliu mai le Nofoa a pule, ia te oe le Alataua"]];
+        $content['id'] = $id +1;
+        $content['motu_id'] = $id;
+        $content['itumalo_id'] = $this->salega_id;
+        $content['name'] = "Vaega";
+        $content['content'][] = ["Vaega" => 
+            ["Susu mai le paia i le nofo fealoa'i ia Sa Asiata (Asiatā)",
+            "lau tofa le vaa i malae le na tausi vaa tele",
+            "Susu mai le vaa i ti",
+            "Susu mai le suafa Fauatea ma le aiga sa Valomua",
+            "Maliu mai le Nofoa a pule, ia te oe le Alataua"]];
         return $content;
     }
     
