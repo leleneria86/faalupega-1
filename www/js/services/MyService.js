f56a0bbca71863274a296ba1d8cb2a5c1fe08f93
fululele <37724798+fululele@users.noreply.github.com>
2019-02-08 15:51:15 -0700

    Update MyService.js

--- www/js/services/MyService.js
+++ www/js/services/MyService.js
@@ -1,12 +1,87 @@
+// var myApp = angular.module('MyApp');
+
+// myApp.factory('MyService', function($http)
+// {
+// 	var service = {};
+
+//     service.load = function() {
+
+//         return $http.get('/services/testapi.php').then(
+
+//             function(result) {
+
+//                 if (result.data) {
+
+//                     var data = result.data;
+
+//                     return data;
+//                 } else {
+
+//                     return $q.reject();
+//                 }
+//             });
+//     };
+
+//     return service;
+// });
+
 var myApp = angular.module('MyApp');
 
-myApp.factory('MyService', function($http)
+myApp.factory('MyService', function($http, $q)
 {
 	var service = {};
 
+    service.registerUser = function(user) {
+
+        return $http.post('/services/api/register.php', user).then(
+            function (result) {
+
+                if (result.data) {
+
+                    var data = result.data;
+
+                    return data;
+                } else {
+
+                    return $q.reject();
+                }
+            });
+    };
+
+    service.loginUser = function(email, pass) {
+
+        return $http.get("/services/api/login.php?email=" + email + "&pass=" + pass).then(
+            function (result) {
+
+                if (result.data) {
+
+                    return result.data;
+                } else {
+
+                    return $q.reject();
+                }
+            });
+    };
+
+    service.checkToken = function(jwt) {
+
+        return $http.get("/services/api/checktoken.php?jwt=" + jwt).then(
+            function (result) {
+
+                if (result.data) {
+
+                    return result.data;
+                } else {
+
+                    return $q.reject();
+                }
+            });
+    };
+
+
     service.load = function() {
 
-        return $http.get('/services/testapi.php').then(
+        return $http.get('/services/api/nuuapi.php').then(
 
             function(result) {
 
@@ -22,6 +97,8 @@ myApp.factory('MyService', function($http)
             });
     };
 
+
     return service;
 });
+
 
