59b61ef1ea09c21cfd594255b3f49e52cae4f16c
fululele <37724798+fululele@users.noreply.github.com>
2019-02-08 15:46:08 -0700

    Create User.js

---
+++ www/js/model/User.js
@@ -0,0 +1,15 @@
+
+
+function User() {
+
+    this.user_jd = null;
+    this.first_name = null;
+    this.last_name = null;
+    this.email = null;
+    this.password = null;
+}
+
+User.prototype = {
+
+
+};
