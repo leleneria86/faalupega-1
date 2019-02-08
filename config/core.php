8a3a460e0ad97b61ee6603fbdc2d3129e2191528
fululele <37724798+fululele@users.noreply.github.com>
2019-02-08 16:07:37 -0700

    Create core.php

---
+++ config/core.php
@@ -0,0 +1,14 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+
+// set your default time-zone
+date_default_timezone_set('America/Denver');
+
+// variables used for jwt
+$key = "tusifaalupega_key";
+$iss = "http://tusifaalupega.com";
+$aud = "http://tusifaalupega.com";
+$iat = 1356999524;
+$nbf = 1357000000;
+?>
