7d6efeae82c224c9b5e3e33b8fdf5f32d875f60b
fululele <37724798+fululele@users.noreply.github.com>
2019-03-01 15:53:54 -0700

    Create landing.php

---
+++ pages/partials/landing.php
@@ -0,0 +1,31 @@
+<?PHP
+
+?>
+
+<section>
+
+    <div class="container">
+        <div class="col-sm-12">
+            <h1 class="title">
+                Tusi Fa'alupega
+            </h1>
+            <h2 class="subtitle">
+                Access your fa'alupega book online for <label>Free!</label>
+            </h2>
+            <div class="content">
+                <ul>
+                    <li>Access Anywhere</li>
+                    <li>Access Quickly</li>
+                    <li>Search Quickly</li>
+                </ul>
+            </div>
+            <div>
+                <a ng-click="gotoLoginView()">
+                    Log In
+                </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
